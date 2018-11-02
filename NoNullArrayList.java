8da72ff021f8b430c64eeec099d43da29c0e2a3f
Camille Sadoff <csadoff00@stuy.edu>
2018-11-02 11:28:50 -0400

    add with index done

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
-public class NoNullArrayList<T> extends ArrayList<T>{
-  public NoNullArrayList() {
 
+public class NoNullArrayList<T> extends ArrayList<T>{
+
+  public NoNullArrayList() {
   }
+
   public NoNullArrayList(int startingCapacity) {
     super(startingCapacity);
   }
+
   public T set(int i, T value) {
     T oldVal = this.get(i);
     if (value == null) {
@@ -16,13 +19,24 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     }
     return oldVal;
   }
+
   public boolean add(T value) {
     if (value == null) {
-      throw new IllegalArgumentException("cannot set null value");
+      throw new IllegalArgumentException("cannot add null value");
     }
     else {
       super.add(value);
     }
     return true;
   }
+
+  public void add(int i, T value) {
+    if (value == null) {
+      throw new IllegalArgumentException("cannot add null value");
+    }
+    else {
+      super.add(i, value);
+    }
+  }
+
 }
