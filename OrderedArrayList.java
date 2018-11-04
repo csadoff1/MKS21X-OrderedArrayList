1c125c7bd7391b38a99992c38e53b588ae5ed99c
Camille Sadoff <csadoff00@stuy.edu>
2018-11-04 16:39:27 -0500

    wrote add with index

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -13,4 +13,10 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     return true;
   }
 
+  public void add(int index, T value) {
+    for (int i=0; i<this.size(); i++) {
+      if (this.get(i).compareTo(value) >= 0) super.add(i, value);
+    }
+  }
+
 }
